7bc92e2793a0b41f72b152e38776729e8e757362
Evelyn <eferistin@gmail.coom>
2017-10-06 19:35:00 -0400

    Users/clents can search job listing via categories: skill, employer, location, position type,  and all. Or via search function criteria.

--- src/TechJobs/Controllers/SearchController.cs
+++ src/TechJobs/Controllers/SearchController.cs
@@ -15,6 +15,32 @@ namespace TechJobs.Controllers
 
         // TODO #1 - Create a Results action method to process 
         // search request and display results
+        public IActionResult Results(string searchType, string searchTerm)
+        {
+            if (searchType == "all") // if the search chosen (searchType) equals to "all" one of the option specified in ListController.columnChoices
+            {
+                List<Dictionary<string, string>> searchword = JobData.FindByValue(searchTerm);
+                ViewBag.columns = ListController.columnChoices;
+                ViewBag.jobs = searchword;
+                return View("Index");
+            }
+            else if (searchType != "all")
+            {
+                List<Dictionary<string, string>> searchword = JobData.FindByColumnAndValue(searchType, searchTerm);
+                ViewBag.columns = ListController.columnChoices;
+                ViewBag.jobs = searchword;
+                return View("Index");
+            }
+            else // if searchType == null, not chosen
+            {
+                string Notfound = "You must specfic a proper search term.";
+                ViewBag.jobs = Notfound;
+                return View("Index");
+            }
+
+            
+        }
+
 
     }
 }
